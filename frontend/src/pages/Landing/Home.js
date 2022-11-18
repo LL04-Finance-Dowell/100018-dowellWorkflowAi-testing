18eccb8415a3e1bad5dde15721a3fc5eedff77e4
Abanoub Milad <59348949+Abanoub-pixel@users.noreply.github.com>
2022-11-18 11:00:57 +0200

    adding sideBar component to home page

--- frontend/src/pages/Landing/Home.js
+++ frontend/src/pages/Landing/Home.js
@@ -1,22 +1,35 @@
 import { Link } from "react-router-dom";
-import { useUserContext } from "../../contexts/UserContext"
-import WorkflowLayout from "../../layouts/WorkflowLayout/WorkflowLayout"
+import SideBar from "../../components/sideBar/SideBar";
+import { useUserContext } from "../../contexts/UserContext";
+import WorkflowLayout from "../../layouts/WorkflowLayout/WorkflowLayout";
 import { dowellLoginUrl } from "../../services/axios";
 import { handleAuthenticationBtnClick } from "../../services/common";
 
 const LandingPage = () => {
-    const { currentUser } = useUserContext();
+  const { currentUser } = useUserContext();
 
-    return <>
-        <WorkflowLayout>
-            <div>
-                Landing
-                {
-                    !currentUser ?  <button><Link to={dowellLoginUrl} onClick={(e) => handleAuthenticationBtnClick(e, dowellLoginUrl)}>Login</Link></button> : <></> 
-                }
-            </div>
-        </WorkflowLayout>
+  return (
+    <>
+      <WorkflowLayout>
+        <div>
+          Landing
+          {!currentUser ? (
+            <button>
+              <Link
+                to={dowellLoginUrl}
+                onClick={(e) => handleAuthenticationBtnClick(e, dowellLoginUrl)}
+              >
+                Login
+              </Link>
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
+      </WorkflowLayout>
+      <SideBar />
     </>
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
