af30c2cd62701d3988222012aba95a55eb9c4011
Abanoub Milad <59348949+Abanoub-pixel@users.noreply.github.com>
2022-11-21 10:21:55 +0200

    adding the new components to the home page

--- frontend/src/pages/Landing/Home.js
+++ frontend/src/pages/Landing/Home.js
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
+import Carousel from "../../components/carousel/Carousel";
+import FlipCard from "../../components/flip-card/FlipCard";
 import SideBar from "../../components/sideBar/SideBar";
+import SmallFlip from "../../components/small-flip/SmallFlip";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+import SearchIcon from "@mui/icons-material/Search";
 import { useUserContext } from "../../contexts/UserContext";
 import WorkflowLayout from "../../layouts/WorkflowLayout/WorkflowLayout";
 import { dowellLoginUrl } from "../../services/axios";
 import { handleAuthenticationBtnClick } from "../../services/common";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 
+import "../Landing/home.css";
 const LandingPage = () => {
   const { currentUser } = useUserContext();
 
@@ -27,7 +35,49 @@ const LandingPage = () => {
           )}
         </div>
       </WorkflowLayout>
-      <SideBar />
+
+      <div className="main">
+        <SideBar />
+        <div className="top-main">
+          <Carousel />
+
+          <div className="sm-flip">
+            <SmallFlip
+              icon={<NotificationsNoneIcon fontSize="large" />}
+              content={"006"}
+              button={"View"}
+              size={"50px"}
+              padding={"25px"}
+              color={"#1abc9c"}
+            />
+            <SmallFlip
+              icon={<InsertDriveFileIcon fontSize="large" />}
+              content={"New"}
+              button={"Create Document"}
+              size={"60px"}
+              padding={"20px"}
+              color={"gray"}
+            />
+            <SmallFlip
+              icon={<SearchIcon fontSize="large" />}
+              content={"Search"}
+              button={"Search Document"}
+              size={"60px"}
+              padding={"20px"}
+              color={"#61ce70"}
+            />
+            <SmallFlip
+              icon={<ManageAccountsIcon fontSize="large" />}
+              content={"Support"}
+              button={"Dowell Knowledge Center"}
+              size={"85px"}
+              padding={"8px"}
+              color={"#c3d6be"}
+            />
+          </div>
+        </div>
+        <FlipCard />
+      </div>
     </>
   );
 };
