dec8efe2f3198026de4257bc0656824a6ac4aa30
WawinyEdwin <wawinyedwin44@gmail.com>
2022-11-24 17:36:37 +0300

    Reduced file size by scraping python app

--- backend/members.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://100014.pythonanywhere.com/api/userinfo/"
-
-def get_members(session_id):
-    payload = {"session_id": session_id}
-    r = requests.post(url=url, data=payload)
-    return r.json()
-
