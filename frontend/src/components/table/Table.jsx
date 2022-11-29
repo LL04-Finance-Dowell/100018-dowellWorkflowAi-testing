2ba71b31a4bd61853480d978d2cfe93c80b4c2a8
Abanoub Milad <59348949+Abanoub-pixel@users.noreply.github.com>
2022-11-29 12:35:34 +0200

    edit

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -3,7 +3,9 @@ import style from "../table/table.module.css";
 const Table = (props) => {
   return (
     <div className={style.table}>
-      <span className={style.header}>{props.header}</span>
+      <span style={{ color: `${props.color}` }} className={style.header}>
+        {props.header}
+      </span>
       <div style={{ backGroundColor: "#e1e1e1" }}>
         {" "}
         <Accordion
@@ -34,7 +36,8 @@ const Table = (props) => {
           bColor="white"
           width="100%"
           margin="0"
-          paddingg="12px 0 7px 0"
+          widthh="100%"
+          paddingg="12px 0 20px 0"
           marg="12px"
           top="0"
           contPad="30px 0 0 20px"
@@ -46,17 +49,18 @@ const Table = (props) => {
           content="1. To be Approved (002)"
           content2="2. Rejected by others (001)"
           color="#7a7a91"
-          contColor="#ff0000"
           bgColor="#E1E1DB"
           dotColor="#E1E1DB"
           bColor="white"
           width="100%"
           margin="0"
-          paddingg="12px 0 7px 0"
+          widthh="100%"
+          paddingg="12px 0 20px 0"
           marg="12px"
           top="0"
           contPad="30px 0 0 20px"
           contPadd="0 0 0 20px"
+          contColor="#ff0000"
         />
       </div>
     </div>
