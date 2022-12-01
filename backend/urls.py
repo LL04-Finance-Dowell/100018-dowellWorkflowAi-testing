90322e81f12ae9314ff1bcc3297dec340d494169
datacenter7 <datacenter7@proton.me>
2022-12-01 15:08:33 +0300

    changes wf

--- backend/urls.py
+++ backend/urls.py
@@ -28,7 +28,7 @@ from .document_management import (
     my_documents,
     create_document,
     rejected_documents,
-    reject_document,
+    # reject_document,
 )
 from .wf_management import (create_workflow)
 
@@ -59,7 +59,7 @@ urlpatterns = [
     path("templates/mine/", template_list, name="my_templates"),
     # Documents----------------------------------------------------
     path("documents/", create_document, name="documents"),
-    path("documents/reject/", reject_document, name="reject_document"),
+    # path("documents/reject/", reject_document, name="reject_document"),
     path(
         "documents/to-sign/",
         documents_to_be_signed,
