3ccbf872b4c0bf8effe4e74927e73c8a6d6c1238
Ayoola <ayo.oloyede16@gmail.com>
2023-01-09 10:50:14 +0100

    Remove registration form from legal implementation

--- frontend/src/services/legalService.js
+++ frontend/src/services/legalService.js
@@ -1,9 +1,5 @@
 import { legalAxiosInstance } from "./axios";
 
-export const registerNewWebsite = async (data) => {
-    return await legalAxiosInstance.post("legalpolicies/", data);
-}
-
 export const getAgreeStatus = async (eventId) => {
     return await legalAxiosInstance.get(`legalpolicies/${eventId}/iagreestatus/`)
 }
