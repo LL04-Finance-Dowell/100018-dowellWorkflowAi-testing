d3b09f2c6c4e74a2d0e13517a799289a86c25ac4
Edwin Odhiambo <wawinyedwin44@gmail.com>
2023-01-26 21:07:59 +0300

    added async

--- database/dowellconnection.py
+++ database/dowellconnection.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import aiohttp
 
 url = "http://100002.pythonanywhere.com/"
 
@@ -31,5 +32,42 @@ def dowellconnection(
         }
     )
     headers = {"content-type": "application/json"}
+    # async with aiohttp.ClientSession() as session:
+    #     async with session.post(url, data=data, headers=headers) as response:
+    #         print(await response.text())
+    #         return await response.text()
     response = requests.request("POST", url, headers=headers, data=data)
     return response.text
+
+
+async def dowellconnection_async(
+    cluster,
+    platform,
+    database,
+    collection,
+    document,
+    team_member_ID,
+    function_ID,
+    command,
+    field,
+    update_field,
+):
+    data = json.dumps(
+        {
+            "cluster": cluster,
+            "platform": platform,
+            "database": database,
+            "collection": collection,
+            "document": document,
+            "team_member_ID": team_member_ID,
+            "function_ID": function_ID,
+            "command": command,
+            "field": field,
+            "update_field": update_field,
+        }
+    )
+    headers = {"content-type": "application/json"}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=data, headers=headers) as response:
+            print(await response.text())
+            return await response.text()
