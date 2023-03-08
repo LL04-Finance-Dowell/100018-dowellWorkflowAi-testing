c70455b9ba126851cecbd5f214b813642d3f4e01
Edwin Odhiambo <wawinyedwin44@gmail.com>
2023-03-08 15:35:19 +0300

    favourite review complete

--- document/save_favorites.py
+++ document/save_favorites.py
@@ -1,26 +1,18 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from database.mongo_db_connection import (
-    get_document_object,
-    get_template_object, get_wf_object
-)
-from database.mongo_db_connection_v2 import save_document
 from .thread_start import FavoriteThread
 
 
 @api_view(["GET"])
-def favorite(request, id, type):
+def favorite(request, item_id, item_type):
     try:
-        if type=="document":
-            FavoriteThread(id, type).start()
-        if type=="template":
-            FavoriteThread(id,type).start()
-        if type=="workflow":
-            FavoriteThread(id, type).start()
+        if type == "document":
+            FavoriteThread(item_id, item_type).start()
+        if type == "template":
+            FavoriteThread(item_id, item_type).start()
+        if type == "workflow":
+            FavoriteThread(item_id, item_type).start()
         return Response(status=status.HTTP_200_OK)
     except RuntimeError:
         return
-        
-    
-    
