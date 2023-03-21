50d8b3811838d7d8d77b261dfbb8e6be799c3c92
Edwin Odhiambo <wawinyedwin44@gmail.com>
2023-03-21 15:56:47 +0300

    Minified model migration

--- app/models.py
+++ app/models.py
@@ -4,20 +4,20 @@ from django.db import models
 
 class FavoriteTemplate(models.Model):
     _id = models.TextField(primary_key=True)
-    company_id = jsonfield.JSONField()
-    template_name = jsonfield.JSONField()
-    favourited_by = models.TextField(max_length=200)
+    company_id = jsonfield.JSONField(default="Company")
+    template_name = jsonfield.JSONField(default="Template")
+    favourited_by = models.TextField(max_length=200, default="User")
 
 
 class FavoriteDocument(models.Model):
     _id = models.TextField(primary_key=True)
-    document_name = jsonfield.JSONField()
-    company_id = jsonfield.JSONField()
-    favourited_by = models.TextField(max_length=200)
+    document_name = jsonfield.JSONField(default="Document")
+    company_id = jsonfield.JSONField(default="Company")
+    favourited_by = models.TextField(max_length=200, default="User")
 
 
 class FavoriteWorkflow(models.Model):
     _id = models.TextField(primary_key=True)
-    company_id = jsonfield.JSONField()
-    workflows = jsonfield.JSONField()
-    favourited_by = models.TextField(max_length=200)
+    company_id = jsonfield.JSONField(default="Company")
+    workflows = jsonfield.JSONField(default="Workflows")
+    favourited_by = models.TextField(max_length=200, default="User")
