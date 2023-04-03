8dc384cda462910656775532142efe701a814f82
Ayoola <ayo.oloyede16@gmail.com>
2023-04-03 20:54:15 +0100

    Use native sticky behaviour instead of custom hook

--- frontend/src/components/setWorkFlowInDocNew/contentMapOfDoc/ContentMapOfDoc.jsx
+++ frontend/src/components/setWorkFlowInDocNew/contentMapOfDoc/ContentMapOfDoc.jsx
@@ -44,9 +44,9 @@ const ContentMapOfDoc = () => {
     isFixedCallback();
   }, [isFixedCallback]); */
 
-  useChangeElementPropertyOnScroll(ref, "position", "fixed", "relative", window.screen.height / 1.54);
-  useChangeElementPropertyOnScroll(ref, "top", "0", "", window.screen.height / 1.54);
-  useChangeElementPropertyOnScroll(ref, "width", "66%", "100%", window.screen.height / 1.54);
+  // useChangeElementPropertyOnScroll(ref, "position", "fixed", "relative", window.screen.height / 1.54);
+  // useChangeElementPropertyOnScroll(ref, "top", "0", "", window.screen.height / 1.54);
+  // useChangeElementPropertyOnScroll(ref, "width", "66%", "100%", window.screen.height / 1.54);
 
   const variants = {
     initial: {
