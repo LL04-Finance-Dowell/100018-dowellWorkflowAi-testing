086c43571377fcd5d6da5a8664a39b6e0af108f7
wawinyedwin44@gmail.com <wawinyedwin44@gmail.com>
2023-04-30 18:33:04 +0300

    Add function desc about our webhook

--- app/views.py
+++ app/views.py
@@ -55,6 +55,7 @@ from .constants import EDITOR_API
 
 @api_view(["POST"])
 def webhook(request):
+    """Pick an event from GH and update our PA-server code"""
     if request.method == "POST":
         repo = git.Repo("/home/100094/100094.pythonanywhere.com")
         origin = repo.remotes.origin
