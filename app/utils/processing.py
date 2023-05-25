8fe344f9a29ddd39b36e7b33d890f0c0163a28c1
Uchechukwu Ottah <coderboy.exe@gmail.com>
2023-05-25 07:28:27 +0100

    Fix: Fixed step processing order bug

--- app/utils/processing.py
+++ app/utils/processing.py
@@ -214,7 +214,7 @@ def verify(
                         status.HTTP_403_FORBIDDEN,
                     )
 
-            if step.get('stepProcessingOrder')  :
+            if step.get('stepProcessingOrder'):
                 if not checks.step_processing_order(
                     order=step.get('stepProcessingOrder'),
                     process_id=process.get('_id'),
