c1c7cf3e3a5e9a272791b0d1f23ee8be34e49029
wawinyedwin44@gmail.com <wawinyedwin44@gmail.com>
2023-06-08 08:30:28 +0300

    Add the new notification API url

--- app/views.py
+++ app/views.py
@@ -38,6 +38,7 @@ from app.utils.mongo_db_connection import (
     get_team,
     get_team_list,
     get_template_list,
+    get_template_object,
     get_wf_list,
     get_wf_object,
     get_wf_setting_object,
@@ -1024,7 +1025,6 @@ def get_completed_documents_by_process(request, company_id, process_id):
 
 @api_view(["POST"])
 def create_workflow_ai_setting(request):
-    # TODO: check if the organization has a setting already if yes, they can only have one so return and tell them that
     if not request.data:
         return Response("You are missing something", status.HTTP_400_BAD_REQUEST)
 
