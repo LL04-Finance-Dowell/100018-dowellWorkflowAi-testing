5bba816d21dc617f1bdd9f038eafccdd15419b80
couzy <courageabudu@gmail.com>
2023-06-08 17:06:51 +0100

    Made changes to public links

--- app/utils/verification.py
+++ app/utils/verification.py
@@ -36,9 +36,10 @@ class Verification:
 
     @staticmethod
     def generate_qrcode(link):
-        qr_path = f"100094.pythonanywhere.com/media/qrcodes/{uuid.uuid4().hex}.png"
-        # qr_path = f"media/qrcodes/{uuid.uuid4().hex}.png"  # On dev
-        qr_code = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+        # qr_path = f"100094.pythonanywhere.com/media/qrcodes/{uuid.uuid4().hex}.png"
+        qr_path = f"media/qrcodes/{uuid.uuid4().hex}.png"  # On dev
+        qr_code = qrcode.QRCode(
+            error_correction=qrcode.constants.ERROR_CORRECT_H)
         qr_code.add_data(link)
         qr_code.make()
         qr_color = "black"
