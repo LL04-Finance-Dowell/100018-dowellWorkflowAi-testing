1fc628607809a5f421f9225214557819f8ded7a7
wawinyedwin44@gmail.com <wawinyedwin44@gmail.com>
2023-06-11 09:33:53 +0300

    Add links object validation

--- app/views.py
+++ app/views.py
@@ -183,7 +183,10 @@ def document_processing(request):
 @api_view(["POST"])
 def get_process_link(request, process_id):
     """get a link process for person having notifications"""
-    links_info = get_links_object_by_process_id(process_id)[0]
+    links_object = get_links_object_by_process_id(process_id)
+    if not links_object:
+        return Response("Verification link unavailable", status.HTTP_400_BAD_REQUEST)
+    links_info = links_object[0]
     user = request.data["user_name"]
     if not links_info:
         return Response(status.HTTP_500_INTERNAL_SERVER_ERROR)
