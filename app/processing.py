1d30ccd03a8b299220b377d592f5a5e2c13979da
wawinyedwin44@gmail.com <wawinyedwin44@gmail.com>
2023-07-01 08:32:52 +0300

    model response to json

--- app/processing.py
+++ app/processing.py
@@ -180,6 +180,7 @@ class HandleProcess(Verification):
         response = requests.post(
             QRCODE_URL, payload, {"Content-Type": "application/json"}
         )
+        response.json()
         master_link = response["masterlink"]
         master_qrcode = response["qrcode_image_url"]
         return master_link, master_qrcode
