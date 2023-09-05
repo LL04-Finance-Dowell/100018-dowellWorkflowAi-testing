03d51485d39ba1b1785b804a92d90f0801696b22
Muhammad Ans <75665100+Muhammadansriaz@users.noreply.github.com>
2023-09-06 01:37:19 +0800

    Base url changed

--- src/httpCommon/httpCommon.js
+++ src/httpCommon/httpCommon.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { extractAuthQueryParamsFromVerificationURL, extractTokenFromVerificationURL } from "../utils/helpers";
 
-export const api_url = "https://workflowai.uxlivinglab.online/v1/";
+export const api_url = "https://100094.pythonanywhere.com/v1/";
 export const auth_url = "https://100014.pythonanywhere.com/api/";
 const auth_url_other = "https://100093.pythonanywhere.com/api/";
 const new_process_api_url = "https://workflowai.uxlivinglab.online/v0.2/";
