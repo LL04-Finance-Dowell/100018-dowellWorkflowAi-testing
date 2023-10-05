417c472a9fee8cfd879b8c710051ac199c887955
WawinyEdwin <wawinyedwin44@gmail.com>
2023-10-05 19:18:55 +0300

    explicitly use the prod version

--- wf_ai_core/settings.py
+++ wf_ai_core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+ENV="PRODUCTION"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
