0b47ddc6580c90acd3faec8b74b22a9a84b872eb
WawinyEdwin <wawinyedwin44@gmail.com>
2024-01-03 17:25:48 +0300

    add conditional GET m/w

--- wf_ai_core/settings.py
+++ wf_ai_core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.http.ConditionalGetMiddleware",
     "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
