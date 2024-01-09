561485558e5bd176381821712e17047ea312cb22
Uchechukwu Ottah <usottah@gmail.com>
2024-01-09 23:14:45 +0100

    Update: Changes to internal processing

--- app/views_v2.py
+++ app/views_v2.py
@@ -288,6 +288,33 @@ class ProcessDetail(APIView):
                 process.update({"links": links_object["links"]})
         process["progress"] = progress
         return Response(process, status.HTTP_200_OK)
+    
+    
+    def put(self, request, process_id):
+        """_summary_
+        Args:
+            request (req): _description_
+            process_id (str): _description_
+        """
+        if not validate_id(process_id):
+            return Response("Something went wrong!", status.HTTP_400_BAD_REQUEST)
+        
+        if not request.query_params.get("step_id"):
+            return Response("Invalid request: step_id is missing", status.HTTP_400_BAD_REQUEST)
+        step_id = int(request.query_params.get("step_id"))
+        step_id -= 1
+        
+        process = single_query_process_collection({"_id": process_id})
+        steps = process.get("process_steps")
+        state = "processing_state"
+        
+        step_content = steps[step_id]
+        if step_content.get("permitInternalWorkflow") == True:
+            step_content.update({"workflows": "Testing 234"})
+            update_process(process_id, steps, state)
+            return Response(process, status.HTTP_200_OK)
+        else:
+            return Response("Internal workflow is not permitted in this step", status.HTTP_403_FORBIDDEN)
 
 
 class ProcessLink(APIView):
