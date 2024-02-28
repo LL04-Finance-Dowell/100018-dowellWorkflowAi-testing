54d9ceac720fc088b86c966f518f8dfb8f4b7426
WawinyEdwin <wawinyedwin44@gmail.com>
2024-02-28 19:17:41 +0300

    not required

--- backend/wf_ai_core/settings.py
+++ backend/wf_ai_core/settings.py
@@ -65,7 +65,6 @@ ROOT_URLCONF = "wf_ai_core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "frontend/build")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
