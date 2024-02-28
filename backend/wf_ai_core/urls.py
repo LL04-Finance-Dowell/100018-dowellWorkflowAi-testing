4713feaec3ed5f8ac7d591db9222caa2a5601754
WawinyEdwin <wawinyedwin44@gmail.com>
2024-02-28 19:08:51 +0300

    remove unused imports

--- backend/wf_ai_core/urls.py
+++ backend/wf_ai_core/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from app.views import home, index
 
 schema_view = get_schema_view(
     openapi.Info(
