3ce139612d37b073da97f52e07de37d2011b1f5e
WawinyEdwin <wawinyedwin44@gmail.com>
2024-02-28 12:13:52 +0300

    map the correct port

--- backend/wf_ai_core/settings.py
+++ backend/wf_ai_core/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "frontend/build/static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
