364a5cb5b28a90db12cde036ff10d86526ec4616
Courage <courageabudu@gmail.com>
2024-03-12 22:20:21 +0100

    checks for databases added

--- backend/education/urls.py
+++ backend/education/urls.py
@@ -4,9 +4,10 @@ from education import views
 
 urlpatterns = [
     path("education/", views.HomeView.as_view()),
-    path("education/templates/", views.NewTemplate.as_view()),
+    path("eduation/database", views.DatabaseServices.as_view()).path(
+        "education/templates/", views.NewTemplate.as_view()
+    ),
     path("education/workflows/", views.Workflow.as_view()),
-
     path("education/documents/", views.NewDocument.as_view()),
     path("education/collections/", views.CollectionData.as_view()),
     path("education/processes/", views.ItemProcessing.as_view()),
@@ -14,5 +15,4 @@ urlpatterns = [
     path("education/documents/<str:item_id>/link/", views.DocumentLink.as_view()),
     path("education/documents/<str:item_id>/detail/", views.DocumentDetail.as_view()),
     path("education/documents/<str:company_id>/list/", views.Document.as_view()),
-
 ]
